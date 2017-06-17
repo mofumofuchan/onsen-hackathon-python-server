f21e151ff3bfbef564b0c2d5586e5be3bf8d2c35
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-18 04:31:55 +0900

    enClass analisys-tesntiment.py

--- analysis-sentiment.py
+++ analysis-sentiment.py
@@ -16,44 +16,43 @@ class SentimentAnalyser(object)
                 cnt += 1
         return score, number
 
-if __name__ == "__main__":
-    f = open('pn_ja.dic.txt', 'r')
-    for line in f:
-        line = line.rstrip()
-        x = line.split(':')
-        if abs(float(x[3]))>0:
-            if x[2] == '名詞':
-                nounswords.append(x[0])
-                nounspoint.append(x[3])
-            if x[2] == '動詞':
-                verbswords.append(x[0])
-                verbspoint.append(x[3])
-            if x[2] == '形容詞':
-                adjswords.append(x[0])
-                adjspoint.append(x[3])
-            if x[2] == '副詞':
-                advswords.append(x[0])
-                advspoint.append(x[3])
-    f.close()
+    def __call__(self, text):
+        f = open('pn_ja.dic.txt', 'r')
+        for line in f:
+            line = line.rstrip()
+            x = line.split(':')
+            if abs(float(x[3]))>0:
+                if x[2] == '名詞':
+                    nounswords.append(x[0])
+                    nounspoint.append(x[3])
+                if x[2] == '動詞':
+                    verbswords.append(x[0])
+                    verbspoint.append(x[3])
+                if x[2] == '形容詞':
+                    adjswords.append(x[0])
+                    adjspoint.append(x[3])
+                if x[2] == '副詞':
+                    advswords.append(x[0])
+                    advspoint.append(x[3])
+        f.close()
  
-    tagger = MeCab.Tagger('-Ochasen')
-    node = tagger.parseToNode(str(text))
-    while node:
-        if node.feature.split(",")[0] == '名詞':
-            nouns.append(node.surface)
-        if node.feature.split(",")[0] == '動詞':
-            verbs.append(node.surface)
-        if node.feature.split(",")[0] == '形容詞':
-            adjs.append(node.surface)
-        if node.feature.split(",")[0] == '副詞':
-            advs.append(node.surface)
-        node = node.next
+        tagger = MeCab.Tagger('-Ochasen')
+        node = tagger.parseToNode(str(text))
+        while node:
+            if node.feature.split(",")[0] == '名詞':
+                nouns.append(node.surface)
+            if node.feature.split(",")[0] == '動詞':
+                verbs.append(node.surface)
+            if node.feature.split(",")[0] == '形容詞':
+                adjs.append(node.surface)
+            if node.feature.split(",")[0] == '副詞':
+                advs.append(node.surface)
+            node = node.next
  
-    score = number = 0
-    score, number = analyze(nouns,nounswords,nounspoint,score,number)
-    score, number = analyze(verbs,verbswords,verbspoint,score,number)
-    score, number = analyze(adjs,adjswords,adjspoint,score,number)
-    score, number = analyze(advs,advswords,advspoint,score,number)
-    if number > 0:
-        print(score/number)
-
+        score = number = 0
+        score, number = analyze(nouns,nounswords,nounspoint,score,number)
+        score, number = analyze(verbs,verbswords,verbspoint,score,number)
+        score, number = analyze(adjs,adjswords,adjspoint,score,number)
+        score, number = analyze(advs,advswords,advspoint,score,number)
+        if number > 0:
+            print(score/number)
