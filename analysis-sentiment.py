2fb289174732df8750bc8dd4a4fa5d02c0325534
mofumofuchan <you_oki1@yahoo.co.jp>
2017-06-18 03:59:56 +0900

    Add sentiment-analisys experimentally

---
+++ analysis-sentiment.py
@@ -0,0 +1,63 @@
+import MeCab
+text = "こんにちは。とてもうれしいことが起こりました。本当にびっくりです。うれしい"
+#text = raw_input()
+print(text)
+nouns, verbs, adjs, advs = [], [], [], []
+nounswords, verbswords, adjswords, advswords = [], [], [], []
+nounspoint, verbspoint, adjspoint, advspoint = [], [], [], []
+ 
+def analyze(hinsi, words, point):
+    #品詞分解したwordと辞書のwordが一致するかチェック
+    global score, number
+    for i in hinsi:
+        cnt = 0
+        for j in words:
+            if i == j:
+                print(j, point[cnt])
+                score += float(point[cnt])
+                number += 1
+            cnt += 1
+
+if __name__ == "__main__":
+    #単語感情極性対応表を読み込む
+    f = open('pn_ja.dic.txt', 'r')
+    for line in f:
+        line = line.rstrip()
+        x = line.split(':')
+        if abs(float(x[3]))>0:	#ポイントの調整
+            if x[2] == '名詞':
+                nounswords.append(x[0])
+                nounspoint.append(x[3])
+            if x[2] == '動詞':
+                verbswords.append(x[0])
+                verbspoint.append(x[3])
+            if x[2] == '形容詞':
+                adjswords.append(x[0])
+                adjspoint.append(x[3])
+            if x[2] == '副詞':
+                advswords.append(x[0])
+                advspoint.append(x[3])
+    f.close()
+ 
+    #mecabで文章を品詞分解する
+    tagger = MeCab.Tagger('-Ochasen')
+    node = tagger.parseToNode(text.encode('utf-8'))
+    while node:
+        if node.feature.split(",")[0] == '名詞':
+            nouns.append(node.surface)
+        if node.feature.split(",")[0] == '動詞':
+            verbs.append(node.surface)
+        if node.feature.split(",")[0] == '形容詞':
+            adjs.append(node.surface)
+        if node.feature.split(",")[0] == '副詞':
+            advs.append(node.surface)
+        node = node.next
+ 
+    #辞書を使って感情分析する
+    score = number = 0
+    analyze(nouns,nounswords,nounspoint)
+    analyze(verbs,verbswords,verbspoint)
+    analyze(adjs,adjswords,adjspoint)
+    analyze(advs,advswords,advspoint)
+    if number > 0:
+        print(score/number)
